a200eb534af7b422fceca644e73ab07ccd11ceda
Yurchenko-Anton <109608021+Yurchenko-Anton@users.noreply.github.com>
2023-01-13 19:27:08 +0200

    [2] Send drive location to user via kafka (#2)
    send driver location to user via kafka

---
+++ src/main/java/com/example/action/service/DriverLocationProcessor.java
@@ -0,0 +1,34 @@
+package com.example.action.service;
+
+import com.example.action.dto.Order;
+import com.example.action.repository.OrderDriverRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import static com.example.action.config.KafkaConfig.PRODUCER_DRIVER_LOCATION_TO_CLIENT_TOPIC;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class DriverLocationProcessor {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final OrderDriverRepository orderDriverRepository;
+
+    public void processDriverLocationMessage(String id, String msg) {
+        try {
+            Order order = orderDriverRepository.findOrderByDriverId(Long.parseLong(id)).getBody();
+            if (order != null) {
+                sendDriverLocationToClient(order.getPassengerId().toString(), "Driver : " + id + " on the street : " + msg);
+            } else log.info("Driver " + id + " dosen't have an active order");
+        } catch (Exception e) {
+            log.error(e.toString());
+        }
+    }
+
+    private void sendDriverLocationToClient(String passengerId, String msg) {
+        kafkaTemplate.send(PRODUCER_DRIVER_LOCATION_TO_CLIENT_TOPIC, passengerId, msg);
+    }
+}
